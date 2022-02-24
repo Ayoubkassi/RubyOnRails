916b9a9b49f9feddc0bd31f4b161101f03ad854b
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-02-24 22:11:03 +0100

    add crud functionnality using : rails g scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :friends
   #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get 'home/about'
   root "home#index"
 end
